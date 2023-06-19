6fdf5a0c324349e830552e9c81aa5199ab4ea3f6
alek <wait82985@gmail.com>
2023-06-19 19:27:38 +0600

    initial commit. 2 test cases

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void combinationSum4Test1() {
+        int[] nums = {1, 2, 3};
+        int target = 4;
+        int output = 7;
+        Assert.assertEquals(7, new Solution().combinationSum4(nums, target));
+    }
+
+    @Test
+    public void combinationSum4Test2() {
+        int[] nums = {1, 2, 3};
+        int target = 4;
+        int output = 7;
+        Assert.assertEquals(7, new Solution().combinationSum4(nums, target));
+    }
+
+    @Test
+    public void combinationSum4Test3() {
+        int[] nums = {1, 2, 3};
+        int target = 4;
+        int output = 7;
+        Assert.assertEquals(7, new Solution().combinationSum4(nums, target));
+    }
+}
