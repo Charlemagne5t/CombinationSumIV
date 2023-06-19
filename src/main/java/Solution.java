b58b77e8c412d7a6ce3ebcb999b52088d07f91c3
alek <wait82985@gmail.com>
2023-06-19 22:18:36 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,30 @@
+import java.util.*;
+
 public class Solution {
     public int combinationSum4(int[] nums, int target) {
-        return 0;
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        return dfs(nums, target, memo);
 
     }
+
+    private int dfs(int[] nums, int target, HashMap<Integer, Integer> memo) {
+        if (memo.containsKey(target)) {
+            return memo.get(target);
+        }
+        if (target < 0) {
+            return 0;
+        }
+        if (target == 0) {
+            return 1;
+        }
+
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result += dfs(nums, target - nums[i], memo);
+        }
+        memo.put(target, result);
+        return result;
+    }
 }
+
+
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -7,22 +7,30 @@ public class SolutionTest {
         int[] nums = {1, 2, 3};
         int target = 4;
         int output = 7;
-        Assert.assertEquals(7, new Solution().combinationSum4(nums, target));
+        Assert.assertEquals(output, new Solution().combinationSum4(nums, target));
     }
 
     @Test
     public void combinationSum4Test2() {
-        int[] nums = {1, 2, 3};
-        int target = 4;
-        int output = 7;
-        Assert.assertEquals(7, new Solution().combinationSum4(nums, target));
+        int[] nums = {9};
+        int target = 3;
+        int output = 0;
+        Assert.assertEquals(output, new Solution().combinationSum4(nums, target));
     }
-
     @Test
     public void combinationSum4Test3() {
-        int[] nums = {1, 2, 3};
-        int target = 4;
-        int output = 7;
-        Assert.assertEquals(7, new Solution().combinationSum4(nums, target));
+        int[] nums = {5, 1, 8};
+        int target = 24;
+        int output = 982;
+        Assert.assertEquals(output, new Solution().combinationSum4(nums, target));
     }
+    @Test
+    public void combinationSum4Test4() {
+        int[] nums = {2, 1, 3};
+        int target = 35;
+        int output = 1132436852;
+        Assert.assertEquals(output, new Solution().combinationSum4(nums, target));
+    }
+
+
 }
